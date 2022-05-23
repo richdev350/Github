727a8195591829fdb1effb847ab8e0c303c47a86
bigstar888 <goldtrump1015@gmail.com>
2022-05-23 06:46:47 -0700

    Routing Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommitsComponent } from './components/commits/commits.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'repositories/:userName', component: RepositoriesComponent },
+  { path: 'commits/:userName/:repoName', component: CommitsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
