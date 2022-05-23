3b34a11d213646b593e0684ecafc625c85aaac9c
bigstar888 <goldtrump1015@gmail.com>
2022-05-23 06:26:13 -0700

    Created all required API's

---
+++ src/common/data.serviec.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { url } from './app.constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+  public api = url.apiurl;
+
+  constructor(private http : HttpClient) { }
+
+  getAllPublicRepository(userName:any){
+    return this.http.get(this.api+`users/${userName}/repos`)
+  }
+
+  getCommits(userName:any,reponame:any){
+    return this.http.get(this.api+`repos/${userName}/${reponame}/commits`)
+  }
+}
