0fe3af85ef3a87f95d59972acb62146131df5db2
bigstar888 <goldtrump1015@gmail.com>
2022-05-23 06:52:37 -0700

    Repository Page Completed

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from 'src/common/data.service';
 
 @Component({
   selector: 'app-repositories',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
-
-  constructor() { }
+  myrepo : any
+  userName : any
+  constructor(private activeRoute : ActivatedRoute,private dataService : DataService,private route : Router) { }
 
   ngOnInit(): void {
+    this.activeRoute.params.subscribe(res=>{
+      this.userName = res.userName
+      this.dataService.getAllPublicRepository(this.userName ).subscribe(resp=>{
+        this.myrepo = resp
+      })
+    })
+    
+  }
+
+  selectRepo(reponame:any){
+    this.route.navigateByUrl(`/commits/${this.userName}/${reponame}`)
   }
 
 }
